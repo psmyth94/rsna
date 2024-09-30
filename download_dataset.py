728f372972e80b8aa1ffd47e88be7d538d68ddf3
psmyth94 <psmyth1994@gmail.com>
2024-09-30 14:03:56 -0400

    add args for downloading dataset

--- download_dataset.py
+++ download_dataset.py
@@ -1,24 +1,33 @@
-import os
+import argparse
 import zipfile
 
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 
-def main():
+def main(args):
     api = KaggleApi()
     api.authenticate()
 
-    os.makedirs("data", exist_ok=True)
-    name = "rsna-2024-lumbar-spine-degenerative-classification"
-
     # Download a dataset (replace with the dataset you want)
     api.competition_download_files(name, path="data/", force=True, quiet=False)
-
     # unzip the dataset
+
     with zipfile.ZipFile(f"data/{name}.zip", "r") as zip_ref:
-        zip_ref.extractall(f"data/{name}")
+        zip_ref.extractall("data/isic-2024-challenge")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Download dataset")
+    parser.add_argument("--dataset", "-d", type=str, required=True, help="Dataset name")
+    parser.add_argument(
+        "--competition", "-c", type=str, required=True, help="Competition name"
+    )
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    main()
-    # %%
+    args = parse_args()
+    assert (args.dataset and not args.competition) or (
+        not args.dataset and args.competition
+    ), "Please provide either dataset or competition name"
+    main(args)
